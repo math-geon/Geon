55c8196d6ae199840e96425c8b84f71466830d25
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2020-11-13 11:42:38 -0300

    Add server and css

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http)
+const path = require('path')
+const FrontEndPath = path.resolve('..','frontend')
+
+//Permite o envio de dados entre o jogo e o servidor usando o padrão json.
+app.use(express.json());
+//Deixa os arquivos do jogo publicos para que o jogo possa carregar seus arquivos
+app.use(express.static(FrontEndPath));
+//Faz com que quando a pessoa acesse o site ele envie o arquivo do jogo para a pessoa.
+app.get("/", (req, res)=> {
+    res.sendFile(path.resolve(FrontEndPath, 'Geon.html'))
+})
+
+//Liga o servidor.
+http.listen('3000', ()=>{
+    console.log("Connectado com a porta: 3000")
+});
---
+++ frontend/main.js
@@ -0,0 +1,9 @@
+function StartGame() {
+    document.getElementsByClassName("ConnectingScreen")[0].style.display = 'block';
+    document.getElementsByClassName("ProgressProgressBar")[0].style.width = '0vw';
+    ConnectToServer();
+}
+
+function ConnectToServer() {
+
+}
