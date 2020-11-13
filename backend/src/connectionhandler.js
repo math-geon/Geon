c8683581cf18633a9b5c218f0770c5c56eb9b19f
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2020-11-13 17:39:34 -0300

    MainMenu Complete

--- backend/src/connectionhandler.js
+++ backend/src/connectionhandler.js
@@ -1,4 +1,28 @@
 var ActiveRooms = []
+var ids = [];
+var cleaning = false;
+
+function cleaner() {
+    if (!cleaning) {
+        cleaning = true;
+        setTimeout(()=> {
+            ActiveRooms.forEach((element, index) => {
+                element.Players.forEach((element2, index2) => {
+                    if (!ids.includes(element2.Id)) {
+                        ActiveRooms[index].Players.splice(index2,1);
+                    }
+                })
+            })
+            ActiveRooms.forEach((element, index) => {
+                if (element.Players.length === 0) {
+                    ActiveRooms.splice(index,1);
+                }
+            })
+            ids = [];
+            cleaning = false;
+        }, 15000);
+    }
+}
 
 function getRandomString(length) {
     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -11,6 +35,7 @@ function getRandomString(length) {
 
 function reconnect(socket) {
     socket.broadcast.emit('reconnect');
+    cleaner();
 }
 
 function GenerateRoom(socket, Data) {
@@ -24,17 +49,58 @@ function GenerateRoom(socket, Data) {
         Name: "",
     }
     Data = JSON.parse(Data);
-    RoomScheme.Id =getRandomString(7);
+    var RoomId = getRandomString(7);
+    ActiveRooms.forEach((element, index) => {
+        if (element.Id === RoomId) {
+            GenerateRoom(socket, Data);
+            return;
+        }
+    })
+    RoomScheme.Id = RoomId;
     PlayerScheme.Id = Data.UserId;
     RoomScheme.Players.push(PlayerScheme);
     ActiveRooms.push(RoomScheme)
-    console.log(ActiveRooms);
-    socket.emit('RoomId', JSON.stringify({RoomId: RoomScheme.Id}))
+    setTimeout(()=>{
+        socket.emit('RoomId', JSON.stringify({RoomId: RoomScheme.Id}))
+    }, 2000)
+}
+
+function Updater(socket, Data) {
+    Data = JSON.parse(Data);
+    ActiveRooms.forEach((element, index)=> {
+        element.Players.forEach((element2, index2) => {
+            if (element2.Id === Data.oldUserId) {
+                ActiveRooms[index].Players[index2].Id = Data.userId;
+                ids.push(Data.userId);
+                cleaner();
+            }
+        })
+    })
+}
+
+function verifyer(socket, executer, Data) {
+    Data = JSON.parse(Data);
+    var RoomContainsUser = false;
+    ActiveRooms.forEach((element, index)=>{
+        if (element.Id === Data.RoomId) {
+            ActiveRooms[index].Players.forEach((element2, index2)=>{
+                if (element2.Id === Data.UserId) {
+                    RoomContainsUser = true;
+                }
+            })
+        }    
+    })
+    if (RoomContainsUser) {
+        executer(socket, Data)
+    } else {
+        socket.emit('ConnectionFailed');
+    }
 }
 
 module.exports = (io) => {
     io.on('connection', (socket) => {
-        socket.on('GenerateRoom', (Data) => {GenerateRoom(socket, Data)})
+        socket.on('GenerateRoom', (Data) => {GenerateRoom(socket, Data)});
         socket.on('disconnect', () => {reconnect(socket)});
+        socket.on('Reconnector', (Data) => {Updater(socket, Data)});
     });
 };
--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,19 @@
 var CurrentRoom = '';
+var UserId = getRandomString(12);
+
+function Loaded() {
+
+    socket.on('reconnect', ()=>{
+       newUserId = getRandomString(12);
+        oldUserId = UserId
+        socket.emit('Reconnector', JSON.parse({oldUserId: oldUserId, userId: newUserId}));
+        UserId = newUserId;
+    })
+
+    socket.on('ConnectionFailed', () => {
+        document.getElementsByClassName('ErrorScreen')[0].style.display = 'block';
+    })
+}
 
 function getRandomString(length) {
     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -28,13 +43,16 @@ function RoomIdHandler() {
 }
 
 function ProgressbarHandler(Name, Max, Percentage) {
+    if (Percentage === "100") { Percentage = "98"}
     Value = (Percentage/100)*Max;
     AnimationTime = 0;
     function Animation() {
         setTimeout(()=>{
             if (AnimationTime > 100) {return;}
             AnimationTime += 2;
-            document.getElementsByClassName(Name)[0].style.width = ((AnimationTime/100)*Value)+'vw';
+            if (((AnimationTime/100)*Value) > document.getElementsByClassName(Name)[0].style.width.toString().substring(0,document.getElementsByClassName(Name)[0].style.width.toString().length-2)) {
+                document.getElementsByClassName(Name)[0].style.width = ((AnimationTime/100)*Value)+'vw';
+            }
             Animation()
         }, 1)
     }
@@ -45,11 +63,14 @@ function CreateGame() {
     document.getElementsByClassName("MenuBackground")[0].style.display = 'none';
     document.getElementsByClassName("ConnectingScreen")[0].style.display = 'block';
     document.getElementsByClassName("ProgressProgressBar")[0].style.width = '0vw';
-    socket.emit('GenerateRoom', JSON.stringify({UserId: getRandomString(12)}));
-    ProgressbarHandler("ProgressProgressBar", "49.5", "35");
+    socket.emit('GenerateRoom', JSON.stringify({UserId: UserId}));
+    ProgressbarHandler("ProgressProgressBar", "49.5", "75");
     socket.on('RoomId', (Data) => {
         Data = JSON.parse(Data)
         CurrentRoom = Data.RoomId;
-        ProgressbarHandler("ProgressProgressBar", "49.5", "65");
+        ProgressbarHandler("ProgressProgressBar", "49.5", "100");
+        setTimeout(()=>{
+            document.getElementsByClassName("Menus")[0].style.display = 'none';
+        },2000)
     })
 }
