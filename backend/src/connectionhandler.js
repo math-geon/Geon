40f43680c50e77364058a8818ec4f0417365c3a4
Space_Inteprise <spaceinterpriseofficial@gmail.com>
2020-11-13 14:00:35 -0300

    String String String

--- backend/src/connectionhandler.js
+++ backend/src/connectionhandler.js
@@ -1,9 +1,36 @@
+var ActiveRooms = []
+
+const RoomScheme = {
+    Id: '',
+    Players : [],
+}
+
+const PlayerScheme = {
+    Id: "",
+    Name: "",
+}
+
+function getRandomString(length) {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
+
 function reconnect(socket) {
     socket.broadcast.emit('reconnect');
 }
 
+function GenerateRoom(socket) {
+    let RoomId = getRandomString(7)
+
+}
+
 module.exports = (io) => {
     io.on('connection', (socket) => {
+        socket.on('GenerateRoom', () => {GenerateRoom(socket)})
         socket.on('disconnect', () => {reconnect(socket)});
     });
 };
--- frontend/main.js
+++ frontend/main.js
@@ -1,15 +1,24 @@
 function StartGame() {
+    document.getElementsByClassName("MenuBackground")[0].style.display = 'none';
     document.getElementsByClassName("ConnectingScreen")[0].style.display = 'block';
     document.getElementsByClassName("ProgressProgressBar")[0].style.width = '0vw';
-    ConnectToServer();
+    socket.emit('EnterRoom')
 }
 
 function RoomIdHandler() {
     if (document.getElementsByClassName("RoomId")[0].value.length > 7) {
         document.getElementsByClassName("RoomId")[0].value = document.getElementsByClassName("RoomId")[0].value.slice(0,7)
     }
+    if (document.getElementsByClassName("RoomId")[0].value.length === 7) {
+        document.getElementsByClassName("PlayButton")[0].disabled = false;
+    } else {
+        document.getElementsByClassName("PlayButton")[0].disabled = true;
+    }
 }
 
-function ConnectToServer() {
-    socket.emit('connection')
+function CreateGame() {
+    document.getElementsByClassName("MenuBackground")[0].style.display = 'none';
+    document.getElementsByClassName("ConnectingScreen")[0].style.display = 'block';
+    document.getElementsByClassName("ProgressProgressBar")[0].style.width = '0vw';
+    socket.emit('GenerateRoom');
 }
