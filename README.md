4ca1a3d6ab7214f2069feb6539af035757eb143b
Space_Interprise <44732812+emanuelfranklyn@users.noreply.github.com>
2022-01-26 19:43:19 -0300

    Testado vulnerabilidade de UserID corrigido bug de verificação do dado
    Co-Authored-By: Lobo Metalúrgico <43734867+lobometalurgico@users.noreply.github.com>

--- README.md
+++ README.md
@@ -7,16 +7,16 @@
 - [X] Remover efeito de particulas;
 - [X] Adicionar opção para desativar leitor de libras (Desativar auto ligamento da função);
 - [X] Corrigir vulnerabilidade onde o server não verifica o nome do usuario antes de enviar para os outros clients;
-- [ ] Testar correção de vulnerabilidade de UserID;
-- [ ] Não enviar o UserID para todos os Clients conectados;
-- [ ] Corrigir bug onde o server não verifica se o usuario rodou o dado quando recebe uma resposta;
+- [X] Testar correção de vulnerabilidade de UserID;
+- [X] Corrigir bug onde o server não verifica se o usuario rodou o dado quando recebe uma resposta;
 - [ ] Corrigir bug que permite duas pessoas com o mesmo nome de usuario;
-- [ ] Possivel Feature: Mensagem de aviso ao clicar no dado sem ser sua vez;
-- [ ] Possivel Feature: Alterar o nome de usuario durante a partida;
+- [ ] Feature: Alterar o nome de usuario durante a partida;
+- [ ] Feature: Botão para reconectar na tela de conexão perdida
+- [ ] Feature: Mensagem de aviso ao clicar no dado sem ser sua vez;
+- [ ] Feature: Adicionar efeitos sonoros;
 - [ ] Possivel ideia: Full-Rewrite em Typescript e Next.JS;
 - [ ] Possivel ideia: Não usar Socket.IO;
 - [ ] Possivel ideia: Re-desenhar menu principal do jogo;
-- [ ] Botão para reconectar na tela de conexão perdida
 
 Um jogo simples feito usando:
 
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http)
-const path = require('path')
-const FrontEndPath = path.resolve('..','frontend')
+const io = require('socket.io')(http);
+const path = require('path');
+const FrontEndPath = path.resolve(__dirname, '..', 'frontend');
 const connection = require('./src/connectionhandler');
 
-//Permite o envio de dados entre o jogo e o servidor usando o padrão json.
+// Permite o envio de dados entre o jogo e o servidor usando o padrão json.
 app.use(express.json());
-//Deixa os arquivos do jogo publicos para que o jogo possa carregar seus arquivos
+// Deixa os arquivos do jogo publicos para que o jogo possa carregar seus arquivos
 app.use(express.static(FrontEndPath));
-//Faz com que quando a pessoa acesse o site ele envie o arquivo do jogo para a pessoa.
-app.get("/", (req, res)=> {
-    res.sendFile(path.resolve(FrontEndPath, 'Geon.html'))
-})
-//inicia o socket
+// Faz com que quando a pessoa acesse o site ele envie o arquivo do jogo para a pessoa.
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(FrontEndPath, 'Geon.html'));
+});
+// inicia o socket
 connection(io);
 
-//Liga o servidor.
-http.listen('3000', ()=>{
-    console.log("Conectado com a porta: 3000")
+// Liga o servidor.
+http.listen('3000', () => {
+  console.log('Conectado com a porta: 3000');
 });
